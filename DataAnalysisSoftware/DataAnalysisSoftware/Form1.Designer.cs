3099117b83a8e21522593b564aea234be5a14c02
leeraaz <limhzn@gmail.com>
2018-11-13 14:50:53 +0545

    First version of data analysis software. Loading a file into data grid view

---
+++ DataAnalysisSoftware/DataAnalysisSoftware/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace DataAnalysisSoftware
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.aboutToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.fileReading = new System.Windows.Forms.RichTextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.openFiles = new System.Windows.Forms.OpenFileDialog();
+            this.dataView = new System.Windows.Forms.DataGridView();
+            this.menuStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(12, 42);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 0;
+            this.btnLoad.Text = "Load";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(422, 27);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(75, 23);
+            this.btnCancel.TabIndex = 1;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.aboutToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(506, 24);
+            this.menuStrip1.TabIndex = 3;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // aboutToolStripMenuItem
+            // 
+            this.aboutToolStripMenuItem.Name = "aboutToolStripMenuItem";
+            this.aboutToolStripMenuItem.Size = new System.Drawing.Size(52, 20);
+            this.aboutToolStripMenuItem.Text = "About";
+            // 
+            // fileReading
+            // 
+            this.fileReading.Location = new System.Drawing.Point(12, 71);
+            this.fileReading.Name = "fileReading";
+            this.fileReading.Size = new System.Drawing.Size(485, 269);
+            this.fileReading.TabIndex = 4;
+            this.fileReading.Text = "";
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // dataView
+            // 
+            this.dataView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataView.Location = new System.Drawing.Point(23, 372);
+            this.dataView.Name = "dataView";
+            this.dataView.Size = new System.Drawing.Size(438, 177);
+            this.dataView.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(506, 688);
+            this.Controls.Add(this.dataView);
+            this.Controls.Add(this.fileReading);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem aboutToolStripMenuItem;
+        private System.Windows.Forms.RichTextBox fileReading;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.OpenFileDialog openFiles;
+        private System.Windows.Forms.DataGridView dataView;
+    }
+}
+
---
+++ DataAnalysisSoftware/DataAnalysisSoftware/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DataAnalysisSoftware
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            bool flag = this.openFiles.ShowDialog() != DialogResult.Cancel;
+            if (flag)
+            {
+                string data = "";
+                int a = 0;
+                int b = 0;
+                int totalRows = 0;
+                int rows = 0;
+
+                dataView.ColumnCount = 6;
+                dataView.Columns[0].Name = "Heart Rates";
+                dataView.Columns[1].Name = "Speed";
+                dataView.Columns[2].Name = "Cadence";
+                dataView.Columns[3].Name = "Altitude";
+                dataView.Columns[4].Name = "Power";
+                dataView.Columns[5].Name = "Power Balance and Pedaling Index";
+
+                StreamReader streamReader = new StreamReader(openFiles.FileName);
+                string[] filelines = File.ReadAllLines(openFiles.FileName);
+                foreach (string line in filelines)
+                {
+                    if (line.Equals("[HRData]"))
+                    {
+                        a = b;
+                    }
+                    if (a > 0)
+                    {
+                        data += line + "\r\n";
+                    }
+                    b++;
+                    dataView.AllowUserToAddRows = false;
+                    string text = "";
+                    for (text = streamReader.ReadLine(); text != null; text = streamReader.ReadLine())
+                    {
+                        string[] array = text.Split('\t');
+                        dataView.Rows.Add(array);
+                    }
+                    //fileReading.Text = b + data;
+
+                    //int s = totalRows - rows;
+                    //string[] arrayData = new string[s];
+
+                    //string heart = "";
+                    //string speed = "";
+                    //string cadence = "";
+                    //string altitude = "";
+                    //string power = "";
+                    //string pbpi = "";
+                    //if (rows != 0)
+                    //{
+                    //    foreach (string line in filelines)
+                    //    {
+                    //        if (rows < b)
+                    //        {
+                    //            data += line + "\r\n";
+                    //            string[] words = line.Split('\t');
+                    //            for (int x = 0; x < words.Length; x++)
+                    //            {
+                    //                if (x == 0) heart = words[x];
+                    //                else if (x == 1) speed = words[x];
+                    //                else if (x == 2) cadence = words[x];
+                    //                else if (x == 3) altitude = words[x];
+                    //                else if (x == 4) power = words[x];
+                    //                else pbpi = words[x];
+                    //            }
+                    //            string[] row_val = new string[] { heart, speed, cadence, altitude, power, pbpi };
+                    //            dataView.Rows.Add(row_val);
+                    //        }
+                    //        else b++;
+                    //    }
+                    //}
+                    fileReading.Text = data;
+                }
+            }
+        }
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
