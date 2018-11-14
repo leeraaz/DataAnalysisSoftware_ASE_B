1c6d8cad7138207f8b21278400e01332c2a15209
leeraaz <limhzn@gmail.com>
2018-11-14 15:41:34 +0545

    Loaded data into grid view as well as transfered data of one data grid view into another for ploting a graph.

---
+++ DataAnalysisSoftware/DataAnalysisSoftware/Dashbord.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace DataAnalysisSoftware
+{
+    public partial class Dashbord : Form
+    {
+        public Dashbord()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            //selection of file
+            bool flag = this.openFiles.ShowDialog() != DialogResult.Cancel;
+            if (flag)
+            {
+                string data = "";
+                int rows = 0;
+                int a = 0;
+                int b = 0;
+
+                StreamReader streamReader = new StreamReader(openFiles.FileName);
+                string[] filelines = File.ReadAllLines(openFiles.FileName);// reading text file line by lines
+
+                //filtering data to get only HRData
+                foreach (string line in filelines)
+                {
+                    if (line.Equals("[HRData]"))
+                    {
+                        rows = a;
+                    }
+                    a++;
+                }
+                //fileReading.Text = b + data;
+
+                //loading data into grid view.
+                dataView.AllowUserToAddRows = false;
+                dataView.ColumnCount = 6;
+                dataView.Columns[0].Name = "Heart Rates";
+                dataView.Columns[1].Name = "Speed";
+                dataView.Columns[2].Name = "Cadence";
+                dataView.Columns[3].Name = "Altitude";
+                dataView.Columns[4].Name = "Power";
+                dataView.Columns[5].Name = "Power Balance and Pedaling Index";
+
+                if (rows != 0)
+                {
+                    foreach (string line in filelines)
+                    {
+                        if (rows < b)
+                        {
+                            data += line + "\r\n";
+                            //MessageBox.Show(data, "Messagae");
+                            string text = line;
+                            // string[] text = line.Split('\t');
+                            try
+                            {
+                                for (int x = 0; x < text.Length; x++)
+                                {
+                                    string[] array = text.Split(new char[] { '\t' });
+                                    dataView.Rows.Add(array);
+                                }
+                            }
+                            catch (Exception)
+                            {
+                                MessageBox.Show("Error occured.", "Error");
+                            }
+                        }
+                        else b++;
+                    }
+                }
+                fileReading.Text = data;
+            }
+        }
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void btnGraph_Click(object sender, EventArgs e)
+        {
+            graphImage gi = new graphImage();
+            gi.Show();
+            gi.graphDataView.AllowUserToAddRows = false;
+            gi.graphDataView.ColumnCount = 6;
+            gi.graphDataView.Columns[0].Name = "Heart Rates";
+            gi.graphDataView.Columns[1].Name = "Speed";
+            gi.graphDataView.Columns[2].Name = "Cadence";
+            gi.graphDataView.Columns[3].Name = "Altitude";
+            gi.graphDataView.Columns[4].Name = "Power";
+            gi.graphDataView.Columns[5].Name = "Power Balance and Pedaling Index";
+            foreach (DataGridViewRow row in dataView.Rows)
+            {
+                
+                int n = gi.graphDataView.Rows.Add();
+
+                gi.graphDataView.Rows[n].Cells[0].Value = row.Cells[0].Value;
+                gi.graphDataView.Rows[n].Cells[1].Value = row.Cells[1].Value;
+                gi.graphDataView.Rows[n].Cells[2].Value = row.Cells[2].Value;
+                gi.graphDataView.Rows[n].Cells[3].Value = row.Cells[3].Value;
+                gi.graphDataView.Rows[n].Cells[4].Value = row.Cells[4].Value;
+                gi.graphDataView.Rows[n].Cells[5].Value = row.Cells[5].Value;
+            }
+        }
+    }
+}
---
+++ DataAnalysisSoftware/DataAnalysisSoftware/graphImage.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace DataAnalysisSoftware
+{
+    partial class graphImage
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.graphDataView = new System.Windows.Forms.DataGridView();
+            this.zedGraphControl1 = new ZedGraph.ZedGraphControl();
+            this.btnClose = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.graphDataView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // graphDataView
+            // 
+            this.graphDataView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.graphDataView.Location = new System.Drawing.Point(12, 367);
+            this.graphDataView.Name = "graphDataView";
+            this.graphDataView.Size = new System.Drawing.Size(664, 286);
+            this.graphDataView.TabIndex = 6;
+            // 
+            // zedGraphControl1
+            // 
+            this.zedGraphControl1.IsShowPointValues = false;
+            this.zedGraphControl1.Location = new System.Drawing.Point(12, 25);
+            this.zedGraphControl1.Name = "zedGraphControl1";
+            this.zedGraphControl1.PointValueFormat = "G";
+            this.zedGraphControl1.Size = new System.Drawing.Size(664, 336);
+            this.zedGraphControl1.TabIndex = 7;
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(682, 2);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 8;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // graphImage
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(761, 661);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.zedGraphControl1);
+            this.Controls.Add(this.graphDataView);
+            this.Name = "graphImage";
+            this.Text = "graphImage";
+            ((System.ComponentModel.ISupportInitialize)(this.graphDataView)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        public System.Windows.Forms.DataGridView graphDataView;
+        public ZedGraph.ZedGraphControl zedGraphControl1;
+        public System.Windows.Forms.Button btnClose;
+    }
+}
